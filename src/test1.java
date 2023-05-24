ebe2270a6638b847d14d3905bbd128adaec07212
chavigoel <chavigoel@192.168.1.17>
2023-05-24 12:57:45 +0530

    first commit...

---
+++ src/test1.java
@@ -0,0 +1,15 @@
+
+public class test1 {
+
+	public static void main(String[] args) {
+
+		String hungry = "yes";
+		if (hungry == "yes") {
+			System.out.println("Eat Something...");
+		} else {
+			System.out.println("DO Work...");
+		}
+
+	}
+
+}
