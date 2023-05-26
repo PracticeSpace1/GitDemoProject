339fb62eaeec94016f29b9e770fbfeb79855cb01
chavigoel <chavigoel@192.168.1.17>
2023-05-26 16:17:48 +0530

    Changes have been made

---
+++ src/test2.java
@@ -0,0 +1,15 @@
+
+public class test2 {
+
+	public static void main(String[] args) {
+
+		String hungry = "yes";
+		if (hungry == "yes" || hungry == "y" || hungry == "n") {
+			System.out.println("Eat Something...");
+		} else {
+			System.out.println("DO Work...");
+		}
+
+	}
+
+}
